da47f8ac4c934833694051db8d2681e86f779d07
Alejandro Vivas <alejandrojv17@gmail.com>
2024-11-04 23:09:10 -0300

    chore: refactoring

--- integration.js
+++ integration.js
@@ -2,23 +2,24 @@ const axios = require("axios").default;
 const express = require("express");
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+
 //Dependiendo del modelo de POS
 //const { POSAutoservicio } = require('transbank-pos-sdk');
 const { POSIntegrado } = require('transbank-pos-sdk');
 
 const pos = new POSIntegrado();
 
-// Middleware para poder leer datos JSON en las solicitudes POST
-app.use(express.json());
 // Ruta GET
-
 app.get("/api/last-sale", async (req, res) => {
   try {
-    const result =  pos.lastSale(false);
-    return res.send({
-      result,
-    });
+    if (!pos.isConnected) { // Verificar si el POS está conectado
+      return res.status(500).send("Error: POS no está conectado");
+    }
 
+    const result = await pos.lastSale(false);
+    return res.send({ result });
   } catch (error) {
     return res.send(`Error: ${error.message}`);
   }
@@ -27,27 +28,27 @@ app.get("/api/last-sale", async (req, res) => {
 
 // Iniciar el servidor
 function startServer() {
-      app.listen(port, () => {
-        console.log(`Servidor escuchando en http://localhost:${port}`);
-    });
-};
+  app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+  });
+}
 
 function startApp() {
+  pos.setDebug(true);
 
-    pos.setDebug(true);
-
-    pos.autoconnect() // Esta línea permite busca en todos los puertos si existe uno que tenga conectado un equipo POS y se intenta conectar con el primero que encuentra. 
-        .then((port) => {
-            if (port === false) {
-                console.log('No se encontró ningún POS conectado')
-            }  
-
-            console.log('Connected to PORT: ', port.path)
-            pos.loadKeys() // Cargar llaves
-
-            startServer();
-        })
-        .catch((err) => { console.log('Ocurrió un error inesperado', err) })
-};
+  pos.autoconnect() // Buscar y conectar al POS
+    .then((port) => {
+      if (port === false) {
+        console.log('No se encontró ningún POS conectado');
+      } else {
+        console.log('Connected to PORT:', port.path);
+        pos.loadKeys(); // Cargar llaves
+        startServer();  // Iniciar el servidor después de una conexión exitosa
+      }
+    })
+    .catch((err) => {
+      console.log('Ocurrió un error inesperado', err);
+    });
+}
 
 startApp();
