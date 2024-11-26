533e9cccedbef10a4bb092ff83f5f4845c286706
Alejandro Vivas <ajvivas86@gmail.com>
2024-11-26 15:10:49 -0300

    feat: mapear pos response

--- soap3.js
+++ soap3.js
@@ -1,4 +1,4 @@
-const soap = require('strong-soap').soap;
+const soap = require("strong-soap").soap;
 const consola = require("consola");
 const { POSIntegrado } = require("transbank-pos-sdk");
 
@@ -13,9 +13,9 @@ const DICTIONARY = {
 };
 
 function checkRequest() {
-    const url = './pos.wsdl';
-  const username = 'vpm_abap';
-  const password = 'V@nessit@ntoni@3';
+  const url = "./pos.wsdl";
+  const username = "vpm_abap";
+  const password = "V@nessit@ntoni@3";
   const requestArgs = {
     POSID: "IT205078",
     VKORG: "0110",
@@ -25,67 +25,70 @@ function checkRequest() {
 
   soap.createClient(url, options, (err, client) => {
     if (err) {
-      console.error('Error creando cliente SOAP:', err);
+      console.error("Error creando cliente SOAP:", err);
       return;
     }
-  
+
     client.setSecurity(new soap.BasicAuthSecurity(username, password));
 
+    function processData(
+      posResult,
+      { WERKS, VKORG, UNAME, POSID, DATUM, UZEIT, FUNC }
+    ) {
+      let values = "";
 
-    function processData(posResult, {
-      WERKS, VKORG, UNAME, POSID, DATUM, UZEIT, FUNC
-    }) {
-
-      let values = '';
-
-      Object.keys(posResult).forEach(key => {
+      Object.keys(posResult).forEach((key) => {
         values = values.toString().concat(`${posResult[key]}|`);
       });
 
       const requestArgs = {
-        TOPER: 'R',
-        WERKS,
-        VKORG,
-        UNAME,
-        POSID,
-        DATUM,
-        UZEIT,
-        FUNC,
         RESPONSE: {
-          ITEM: `${values}`
-        }
+          item: [
+            {
+              WERKS: WERKS,
+              VKORG: VKORG,
+              UNAME: UNAME,
+              POSID: POSID,
+              DATUM: DATUM,
+              UZEIT: UZEIT,
+              TOPER: "R",
+              FUNC: FUNC,
+              RESPONSE: `${values}`,
+            },
+          ],
+        },
       };
 
-      console.log('requestArgs__', requestArgs);
+      console.log("requestArgs__", requestArgs);
 
-      /* client.ZRFC_POS_TBK_RESPONSE(requestArgs, async (err, result) => {
+      client.ZRFC_POS_TBK_RESPONSE(requestArgs, async (err, _) => {
         if (err) {
-          console.error('TBK_RESPONSE: Error invocando método:', err);
+          console.error("Error en ZRFC_POS_TBK_RESPONSE:", err);
           return;
         }
-        console.log('TBK_RESPONSE: Resultado del método:', result);
-      }); */
+        console.log("ZRFC_POS_TBK_RESPONSE exitoso");
+      });
     }
-  
+
     client.ZRFC_POS_TBK_REQUEST(requestArgs, async (err, result) => {
       if (err) {
-        console.error('TBK_REQUEST: Error invocando método:', err);
+        console.error("Error en ZRFC_POS_TBK_REQUEST:", err);
         return;
       }
-      console.log('TBK_REQUEST: Resultado del método:', result);
+      console.log("ZRFC_POS_TBK_REQUEST exitoso", result);
 
       const { REQUEST, SUBRC } = result;
 
-      if( SUBRC !== 0) return;
+      if (SUBRC !== 0) return;
 
       const posResult = await DICTIONARY[REQUEST.FUNC]();
-      
-      consola.log('___POS_RESULT___', posResult);
+
+      consola.log("___POS_RESULT___", posResult);
 
       processData(posResult, REQUEST);
     });
   });
-};
+}
 
 function startApp() {
   pos.setDebug(true);
@@ -96,7 +99,7 @@ function startApp() {
         consola.error("No se encontró ningún POS conectado");
       } else {
         consola.success("Connected to PORT:", port.path);
-        keys = await pos.loadKeys();
+        // keys = await pos.loadKeys();
         checkRequest();
       }
     })
